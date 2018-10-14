203f207bea0a50179cf68bb7a2baa8c97ce0ae2d
Benjamin Best <benjaminbest@gmx.net>
2018-10-14 11:37:54 +0200

    Added support for Application Insights
    ADDED: Application Insights

---
+++ StockTradingAnalysisWebsite/StockTradingAnalysis.Web/FilterConfig.cs
@@ -0,0 +1,11 @@
+using System.Web.Mvc;
+
+namespace StockTradingAnalysis.Web{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
+        }
+    }
+}
