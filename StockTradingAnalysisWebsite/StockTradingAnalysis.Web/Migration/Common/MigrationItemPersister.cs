0ac1f5bf8ae390031bfe32ce3f8252a193de7ba9
Benjamin Best <benjaminbest@gmx.net>
2020-03-23 15:09:13 +0100

    Removed unused Raven projects

--- StockTradingAnalysisWebsite/StockTradingAnalysis.Web/Migration/Common/MigrationItemPersister.cs
+++ StockTradingAnalysisWebsite/StockTradingAnalysis.Web/Migration/Common/MigrationItemPersister.cs
@@ -16,9 +16,6 @@ namespace StockTradingAnalysis.Web.Migration.Common
 
         public void Set(List<TItem> items)
         {
-//            File.WriteAllText($"{Identifier}.txt", JsonConvert.SerializeObject(item));
-
-
            Stream ms = File.OpenWrite($"{Identifier}.txt");
 
             var formatter = new BinaryFormatter();
@@ -32,13 +29,10 @@ namespace StockTradingAnalysis.Web.Migration.Common
 
         public List<TItem> Get()
         {
-            //var data = File.ReadAllText($"{Identifier}.txt");
-            //return JsonConvert.DeserializeObject<List<TItem>>(data);
-
             var formatter = new BinaryFormatter();
 
             //Reading the file from the server
-            var fs = File.Open($"{Identifier}.txt", FileMode.Open);
+            var fs = File.OpenRead($"{Identifier}.txt");
 
             var obj = formatter.Deserialize(fs);
 
